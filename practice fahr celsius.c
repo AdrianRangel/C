723a4d3807e0689f4a1cdaf19b923ac9f0ef6d5a
AdrianRangel <78566380+AdrianRangel@users.noreply.github.com>
2021-02-11 16:49:42 -0600

    Add files via upload

---
+++ practice fahr celsius.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* print the table of Fahrenheit-Celsius
+    for fahr=0,20,...,300*/
+
+int main()
+        {
+            printf("Fahrenheit and Celsius Table");
+        }
+{
+        int fahr, celsius;
+        int lower, upper, step;
+
+        lower = 0;       /*limitesuperior de la tabla de temperaturas*/
+        upper = 300;     /*limite superior*/
+        step = 20;       /*tamaño del incremento*/
+
+        fahr = lower;
+        while(fahr <= upper)
+        {
+            celsius = 5 * (fahr-32) / 9;
+            printf("%3d\t %6d\n", fahr, celsius);
+            fahr = fahr + step;
+        }
+}
