33e00ac87e81cda191f409ad7c463a9d142e8fd4
AdrianRangel <78566380+AdrianRangel@users.noreply.github.com>
2021-02-18 13:10:26 -0600

    Add files via upload

---
+++ practice fahr celsius, float.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*imprime la tabla Fahrenheit-Celsius
+    for fahr=0,20,...,300, version de punto flotante*/
+
+int main(){
+
+        printf("Fahrenheit and Celsius Table\n");
+
+        float fahr, celsius;
+        int lower, upper, step;
+
+        lower = 0;       //limitesuperior de la tabla de temperaturas//
+        upper = 300;     //limite superior//
+        step = 20;       //tamaño del incremento//
+
+        fahr = lower;
+        while(fahr <= upper)
+
+        {
+
+            celsius = 5 * (fahr-32) / 9;
+            printf("%3.0f %6.1f\n", fahr, celsius);
+            fahr = fahr + step;
+        }
+}
