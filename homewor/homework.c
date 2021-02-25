0bb11bd3b83fcef8d1f49ece83651513491dcd7a
AdrianRangel <78566380+AdrianRangel@users.noreply.github.com>
2021-02-25 14:23:28 -0600

    Add files via upload

---
+++ homewor/homework.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <ctype.h>
+//NO CALIFICAR
+
+int main() {
+    int i = 0;
+    int c = 0;
+    int space = 0;
+    char stro[10] = {"strong"};
+    char scrip[10] = {"script"};
+
+{
+    while ((i == 6 && i < 10)){
+        if (isspace(c))
+            space = 1;
+        else
+        {
+        if (space) {
+            putchar('_');
+            space = 0;
+        }
+        putchar(c);
+        }
+        if ((i != ))
+    }
+}
+}
+            if (c == ('<')){
+
+            while((c = getchar()) == stro[i]
+                  {
+                    i++;
+                  }
+                if (c == scrip[1])
+                    {
+                    c = getchar();
+                    if scrip[2]
+                    {
+                    c = getchar();
+                    if scrip[3]
+                    {
+                     while ((c = getchar()) != '>')
+                    {
+                     while ((c = getchar()) != '>')
+                    }
+                }
+            }
+        }
+    }
+    if (i == 10){
+            i = 0;
+    }
+    while (c != EOF && c != '>'){
+            c = getchar();
+    }
